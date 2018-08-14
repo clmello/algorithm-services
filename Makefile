7f7859ce4906c26802140e9cc9d79eacede1e05c
Carolina Mello <carolina.mello@corp.globo.com>
2018-08-14 17:44:54 -0300

    tests: fizz buzz algorithm (close #3)

--- Makefile
+++ Makefile
@@ -2,4 +2,4 @@ setup:
 	pip install -r requirements.txt -r requirements_dev.txt
 
 test:
-	python tests/test_algorithms.py
+	nosetests tests
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,7 +1,23 @@
+# python packages
 import unittest
+
+# application
+from algorithm_services.algorithms import fizz_buzz
 
 
 class AlgorithmsTestCase(unittest.TestCase):
-    pass
 
-unittest.main()
+    def tests_should_return_one(self):
+        self.assertEqual(fizz_buzz(3)[0], 1)
+
+    def test_should_return_fizz(self):
+        self.assertEqual(fizz_buzz(3)[2], 'fizz')
+    
+    def test_should_return_buzz(self):
+        self.assertEqual(fizz_buzz(5)[4], 'buzz')
+
+    def test_should_return_fizz_buzz(self):
+        self.assertEqual(fizz_buzz(15)[14], 'fizz_buzz')
+
+    def test_should_return_number(self):
+        self.assertEqual(fizz_buzz(5)[3], 4)
