e60e2c67800144551e69bcfd06c2877bd6ba2084
Carolina Mello <carolina.mello@corp.globo.com>
2018-08-15 16:19:41 -0300

    tests: binary search algorithm (close #4)

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -3,10 +3,12 @@ import unittest
 
 # application
 from algorithm_services.algorithms import fizz_buzz
+from algorithm_services.algorithms import binnary_search
 
 
 class AlgorithmsTestCase(unittest.TestCase):
 
+    # fizz buzz tests
     def tests_should_return_one(self):
         self.assertEqual(fizz_buzz(3)[0], 1)
 
@@ -21,3 +23,13 @@ class AlgorithmsTestCase(unittest.TestCase):
 
     def test_should_return_number(self):
         self.assertEqual(fizz_buzz(5)[3], 4)
+
+    # binarry search tests
+    def test_should_return_position_four(self):
+        self.assertEqual(binnary_search([2, 9, 13, 34, 55], 55, 0, 4), 4)
+
+    def test_should_find_number_position(self):
+        self.assertEqual(binnary_search([2, 3, 6, 9, 13, 16, 55], 55, 0, 6), 6)
+
+    def test_should_not_find_number(self):
+        self.assertEqual(binnary_search([1, 4, 7], 5, 0, 2), -1)
