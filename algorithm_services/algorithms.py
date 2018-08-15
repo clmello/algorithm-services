4372d89f0e65949a4900cecd31459a5dea88ccfe
Carolina Mello <carolina.mello@corp.globo.com>
2018-08-15 16:16:57 -0300

    feat: binary search algorithm

--- algorithm_services/algorithms.py
+++ algorithm_services/algorithms.py
@@ -1,4 +1,6 @@
+# fizz buzz
 def fizz_buzz(size):
+
     output = []
     for number in range(1, size+1):
         if number % 3 == 0 and number % 5 == 0:
@@ -10,3 +12,26 @@ def fizz_buzz(size):
         else:
             output.append(number)
     return output
+
+
+# binnary search
+def binnary_search(numbers_list, number, left, right):
+
+    if right >= left:
+        # get middle of array
+        middle = left + (right - left)/2
+        middle = int(middle)
+
+        if numbers_list[middle] == number:
+            return middle
+
+        # number searched is smaller than the number in the middle
+        elif numbers_list[middle] > number:
+            return binnary_search(numbers_list, number, left, middle-1)
+
+        # number searched is bigger thant the number in the middle
+        else:
+            return binnary_search(numbers_list, number, middle+1, right)
+
+    else:
+        return -1
