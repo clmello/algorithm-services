d61aec55e5f13da45f6b7db2c68defc50596519b
Carolina Mello <carolina.mello@corp.globo.com>
2018-09-28 17:09:13 -0300

    test: add debug and autoreload mode

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,32 @@
+from unittest.mock import patch
+from unittest import TestCase
+
 from tornado.testing import AsyncHTTPTestCase
-from algorithm_services import app
+
+from algorithm_services.app import make_app
+from algorithm_services.app import MainHandler
 
 
 class TestHelloApp(AsyncHTTPTestCase):
 
     def get_app(self):
-        return app.make_app()
+        return make_app()
 
     def test_homepage(self):
         # response = self.fetch('/')
         self.http_client.fetch(self.get_url('/'), self.stop)
         response = self.wait()
         self.assertEqual(response.code, 200)
-        self.assertEqual(response.body, b'Hello')
+        self.assertEqual(response.body, b'Hello World')
+
+
+class AppTestCase(TestCase):
+
+    @patch('algorithm_services.app.Application')
+    def test_make_app(self, application_mock):
+        make_app()
+        application_mock.assert_called_with(
+            handlers=[(r"/", MainHandler)],
+            debug=True,
+            autoreload=True,
+        )
