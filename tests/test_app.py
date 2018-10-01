b0fd757015b07b12b94ec77a0875095c6dff9f3f
Carolina Mello <carolina.mello@corp.globo.com>
2018-10-01 18:17:22 -0300

    test: add option

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,12 +5,14 @@ from tornado.testing import AsyncHTTPTestCase
 
 from algorithm_services.app import make_app
 from algorithm_services.app import MainHandler
+from algorithm_services.options import get_options
 
 
 class TestHelloApp(AsyncHTTPTestCase):
 
     def get_app(self):
-        return make_app()
+        options = get_options('local')
+        return make_app(options)
 
     def test_homepage(self):
         # response = self.fetch('/')
@@ -24,9 +26,20 @@ class AppTestCase(TestCase):
 
     @patch('algorithm_services.app.Application')
     def test_make_app(self, application_mock):
-        make_app()
+        options = get_options('local')
+        make_app(options)
         application_mock.assert_called_with(
             handlers=[(r"/", MainHandler)],
             debug=True,
             autoreload=True,
         )
+
+
+class OptionTestCase(TestCase):
+
+    @patch('algorithm_services.options.options')
+    @patch('algorithm_services.options.parse_config_file')
+    def test_get_options(self, parse_mock, options_mock):
+        options = get_options('local')
+        parse_mock.assert_called_with('./algorithm_services/config/local.conf')
+        self.assertEqual(options, options_mock)
